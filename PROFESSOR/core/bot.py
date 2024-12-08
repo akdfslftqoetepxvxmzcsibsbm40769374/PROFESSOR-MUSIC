2b486a68ea4daa77697be3e64858901388351fe2
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2024-12-08 21:55:13 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- PROFESSOR/core/bot.py
+++ PROFESSOR/core/bot.py
@@ -28,27 +28,18 @@ class PROF(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>๏ {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ ➛</b><u>\n\n๏ ɪᴅ ➛ <code>{self.id}</code>\n๏ ɴᴀᴍᴇ ➛ {self.name}\n๏ ᴜsᴇʀɴᴀᴍᴇ ➛ @{self.username}",
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+        except:
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
-
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                "Please SUHANImote your bot as an admin in your log group/channel."
             )
-            exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
-            
